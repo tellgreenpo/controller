7ccfe39513b0023345c642bbed5386cd110e86e5
Manah7 <mayeul.gasc@orange.fr>
2021-09-18 20:23:33 +0200

    set up PC8 et PC10

---
+++ principal.c
@@ -0,0 +1,22 @@
+#include "stm32f10x.h"
+
+int main(void){
+  RCC->APB2ENR |= (0x01 <<2) | (0x01 << 3) | (0x01 << 4);
+  // PC8 en pull up input
+  GPIOC->CRH &= ~(0xF);
+  GPIOC->CRH |= 0x8;
+  // PC10 en pulldown output
+  GPIOC->CRH &= ~(0xF00);
+  GPIOC->CRH |= 0x1 << 8;
+
+  do
+  {
+    // PC8 IDR 0 ==> PC10 ODR 1
+    // Compare IDR register with 0x0100 to check if the 8th bit is 1
+    if(GPIO->IDR & GPIO_IDR_IDR8){
+    }else{
+      // Set the 10th bot of ODR to 1
+      GPIO->ODR |= 0x1 << 10;
+    }
+  }while(1);
+}
