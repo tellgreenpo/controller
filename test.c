574254cb8035596b7ced4a6d9b6ef263acdfe373
Manah7 <mayeul.gasc@orange.fr>
2021-09-19 17:42:34 +0200

    Corrected some functions and created a test file

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include "MYGPIO_H"
 
+MyGPIO_struct_TypeDef * GPIOStructPtr;
+
 
 int setup(MyGPIO_struct_TypeDef * GPIOStructPtr, char pin, char conf){
   GPIOStructPtr->GPIO_Pin = pin;
@@ -8,10 +10,15 @@ int setup(MyGPIO_struct_TypeDef * GPIOStructPtr, char pin, char conf){
 }
 
 
-
 int main(void){
 
-  MyGPIO_Init(MyGPIO_struct_TypeDef * GPIOStructPtr);
-
+  setup(GPIOStructPtr,1,In_Floating);
+  MyGPIO_Init(GPIOStructPtr);
+  MyGPIO_Set(GPIOStructPtr->GPIO,1);
+  MyGPIO_Reset(GPIOStructPtr->GPIO,1);
+  While(int i=0 < 5){
+    MyGPIO_Toggle(GPIOStructPtr->GPIO,1);
+    sleep(1);
+  }
 
 }
