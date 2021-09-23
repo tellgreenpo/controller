8b4beb857f4815bfec3f99115f1c4c62e105223c
tellgreenpo <mateo1000a@gmail.com>
2021-09-23 20:47:02 +0200

    Interruptio init created and handlers redefined

--- Drivers/Include/Driver_Timer.h
+++ Drivers/Include/Driver_Timer.h
@@ -20,7 +20,27 @@ typedef struct{
 
 void MyTimer_Base_Init( MyTimer_Struct_TypeDef * MyTimer ) ;
 
+
+/*
+*************************************************************************
+				* @brief
+				*				@param -> - TIM_TypeDef * Timer : Timer concerne
+													- char Prio : de 0 a 15
+				*				@Note : La fonction MyTimer_Base_Init doit avoir ete lancee au prealable
+*/
+
+void MyTimer_ActiveIT(TIM_TypeDef * Timer, char Prio);
+
 #define MyTimer_Base_Start(Timer) (Timer->TimId->CR1 |= 0x1)
 #define MyTimer_Base_Stop(Timer)	(Timer->TimId->CR1 |= 0x0)
+
+// TIM1 Update interrupt postition 25
+#define TIM1_UPDATE_INTERRUPT_POS 25;
+// TIM2 interrupt general position 28
+#define TIM2_GENERAL_INTERRUPT_POS 28;
+// TIM3 interrupt general position 29
+#define TIM3_GENERAL_INTERRUPT_POS 29;
+// TIM4 interrupt general position 30
+#define TIM4_GENERAL_INTERRUPT_POS 30;
 
 #endif
