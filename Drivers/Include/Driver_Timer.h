8f433c36c6e2b365cfda877371eda5c95b660863
tellgreenpo <mateo1000a@gmail.com>
2021-10-03 16:53:36 +0200

    Clean organisation

--- Drivers/Include/Driver_Timer.h
+++ Drivers/Include/Driver_Timer.h
@@ -30,25 +30,28 @@ void MyTimer_Base_Init( MyTimer_Struct_TypeDef * MyTimer ) ;
 		*				@Note : La fonction MyTimer_Base_Init doit avoir ete lancee au prealable
 **************************************************************************/
 
-void MyTimer_ActiveIT(TIM_TypeDef * Timer, char Prio, void (*IT_function)(void)) ;
+void MyTimer_ActiveIT(TIM_TypeDef * Timer, char Prio, void (*IT_function)(void)) ; 
 
 void TIM1_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void TIM3_IRQHandler(void);
 void TIM4_IRQHandler(void);
 
-/**
-*	@brief
-	*	@param	....
-	*	@Note		Active le channel specifie suTimerr le timer specifie ???
-	*					la gestion de la configuration I/O n'est pas faite dans cette fonction
-	*					ni le reglage de la periode de la PWM (ARR, PSC)
-*/
-void MyTimer_PWM(TIM_TypeDef * Timer, char Channel);
-
 
 
 #define MyTimer_Base_Start(Timer) (Timer->CR1 |= 0x1)
 #define MyTimer_Base_Stop(Timer)	(Timer->CR1 |= 0x0)
 
+/***************************************************************************
+	*	@brief
+	*	@param	....
+	*	@Note		Active le channel specifie suTimerr le timer specifie ???
+	*					la gestion de la configuration I/O n'est pas faite dans cette fonction
+	*					ni le reglage de la periode de la PWM (ARR, PSC)
+**************************************************************************/
+void MyTimer_PWM(TIM_TypeDef * Timer, char Channel);
+
+void setCycle_PWM(TIM_TypeDef * Timer, char Channel, int cycle) ;
+
 #endif
+
--- Drivers/Src/Driver_Timer.c
+++ Drivers/Src/Driver_Timer.c
@@ -73,28 +73,28 @@ void MyTimer_Base_Init(MyTimer_Struct_TypeDef * Timer){
   	};
   }
 
-
-
-
-
-
+	
+	
+	
+	
+	
   // Create the handlers for each IT
   // redefine handlers from the startup file
   // handler is void->void
   // _Vectors table already exists so just need to redefine
-
-
-
+	
+	
+	
 	  void TIM1_UP_IRQHandler(void){
     // reset bit to close the interrupt request
-    TIM1->SR &= ~TIM_SR_UIF ;
+    TIM1->SR &= ~TIM_SR_UIF ; 
     if(pFuncTIM1 != 0){
       (*pFuncTIM1)();
     }
   }
 
-
-
+	
+	
 	  void TIM2_IRQHandler(void){
     // reset bit to close the interrupt request
     TIM2->SR &= ~TIM_SR_UIF ;
@@ -102,6 +102,8 @@ void MyTimer_Base_Init(MyTimer_Struct_TypeDef * Timer){
       (*pFuncTIM2)();
     }
   }
+		
+	
 
 
   void TIM3_IRQHandler(void){
@@ -119,7 +121,7 @@ void MyTimer_Base_Init(MyTimer_Struct_TypeDef * Timer){
     }
   }
 
-  void MyTimer_PWM(TIM_TypeDef * Timer, char Channel){
+ void MyTimer_PWM(TIM_TypeDef * Timer, char Channel){
     // Creating mask
     uint8_t mask;
     /*
@@ -127,9 +129,9 @@ void MyTimer_Base_Init(MyTimer_Struct_TypeDef * Timer){
     * This bit-field defines the direction of the channel (input/output) as well as the used input.
     * 00: CC1 channel is configured as output
     */
-    mask &= 0;
+    mask = 0 ;
     /*
-    * bit 2
+    * bit 2 - OC1FE
     * An active edge on the trigger input acts like a compare match on CC1 output. Then, OC
     * is set to the compare level independently from the result of the comparison. Delay to sample
     * the trigger input and to activate CC1 output is reduced to 3 clock cycles. OCFE acts only if
@@ -149,7 +151,7 @@ void MyTimer_Base_Init(MyTimer_Struct_TypeDef * Timer){
     * 111 PWM mode 2 ==> In upcounting, channel 1 is inactive as long as TIMx_CNT<TIMx_CCR1 else active.
     *  Active ==> OC1REF = '1'
     */
-    mask |= ((0x01 << 2) | (0x01 << 1) | 0x01)<<4;
+    mask |= ((0x01 << 2) | (0x01 << 1) | 0x0) <<4 ;
     /*
     * bit 7
     * On laisse a 0 parce que jsp on verra
@@ -157,30 +159,35 @@ void MyTimer_Base_Init(MyTimer_Struct_TypeDef * Timer){
 
     // Enable output and polarity config
     // CCER bit 0 ==> output signal to output pin
-    // CCER bit 1 ==> output polairty {0 : active high}
+    // CCER bit 1 ==> output polarity {0 : active high}
     if (Channel == 1){
-      Timer->TimId->CCER |= 0x01;
-      Timer->TimId->CCMR1 |= mask;
+      Timer->CCER |= 0x01;
+      Timer->CCMR1 |= mask;
     }else if(Channel == 2){
-      Timer->TimId->CCER |= 0x01 << 4;
-      Timer->TimId->CCMR1 |= mask<<8;
+      Timer->CCER |= 0x01 << 4;
+      Timer->CCMR1 |= mask<<8;
     }else if (Channel == 3){
-      Timer->TimId->CCER |= 0x01 << 8;
-      Timer->TimId->CCMR2 |= mask;
+      Timer->CCER |= 0x01 << 8;
+      Timer->CCMR2 |= mask;
     }else if(Channel == 4){
-      Timer->TimId->CCER |= 0x01 << 12;
-      Timer->TimId->CCMR2 |= mask<<8;
+      Timer->CCER |= 0x01 << 12;
+      Timer->CCMR2 |= mask<<8;
     }
 
   }
 
-  // modify CCRx
-  // PWM freq iis timer freq
-  // for duty cycle ==> need to know speed for each +1 on counter
-  // and then calculate the CCR value to obaitnthe desired ratio
-  // aka sysclk 72 MHz
-  // T = Timer period
-  // Th = ratio * T
-  // Tl = T-Th
-  // Tl * sysclk freq gives CCRx Value
-  void setDutyCycle_PWM(TIM_TypeDef * Timer,)
+  // Update CCRx
+
+	void setCycle_PWM(TIM_TypeDef * Timer, char Channel, int cycle) {
+		
+		if (Channel == 1) {
+			Timer->CCR1 = (Timer->ARR + 1) * (Timer->PSC + 1) *(1-cycle/100) ; 
+		} else if (Channel==2) {
+			Timer->CCR2 = (Timer->ARR + 1) * (Timer->PSC + 1) *(1-cycle/100) ;  ; 
+		} else if (Channel==3) {
+			Timer->CCR3 = (Timer->ARR + 1) * (Timer->PSC + 1) *(1-cycle/100) ;  ; 
+		} else if (Channel==4) {
+			Timer->CCR4 = (Timer->ARR + 1) * (Timer->PSC + 1) *(1-cycle/100) ;  ;
+		}
+}
+
